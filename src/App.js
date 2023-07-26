5ddcfc073c1836dd06d092b14e3d23094582ba96
Aryan Swaroop <aryanswaroophere@gmail.com>
2023-07-26 17:29:37 +0530

    Last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import './components/comp.css'
+import BelowNav from './components/belowNavbar';
+import Profile from './components/pictureProfile';
+import Page from './components/aboutPage';
+import ProjectPage from './components/projectsPast';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <BelowNav />
+      
+      <Page />
+      <ProjectPage />
+      
+      
     </div>
   );
 }
